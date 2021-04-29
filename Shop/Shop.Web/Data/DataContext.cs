6c85d6467694d0598467612ffd5f54715aeb5a36
jchg12 <68677407+jchg12@users.noreply.github.com>
2021-04-29 16:54:30 -0400

    proyecto

---
+++ Shop/Shop.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<Product> Products { set; get; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
